3cd416474cc6fca2d68806ab8db6f7e4640c0cb1
nidhi2793 <MY_NAME@example.com>
2023-09-03 15:11:42 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
 const http = require("http");
-const fs = require("fs");
-let msg = "";
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-      console.log(err);
-      res.write("<html>");
-      res.write("<head><title>Enter Message</title></head>");
-      res.write(
-        `<body><h1>${data}<h1> </br> <form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>`
-      );
-      res.write("</html");
-      return res.end();
-    });
-  }
-  if (req.url === "/message" && req.method == "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-      msg = message;
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const reqHandler = (req, res) => {
+  if (req.url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      console.log(err);
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(
+        `<body><h1>${data}<h1> </br> <form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>`
+      );
+      res.write("</html");
+      return res.end();
+    });
+  }
+  if (req.url === "/message" && req.method == "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<body><h1>Hello</h1></body>");
+  res.write("</html");
+  res.end();
+};
+
+module.exports = reqHandler;
